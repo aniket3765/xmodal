16c4432123b5bf4f80e91ee95628df98f85085ee
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2025-05-11 17:21:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 function App() {
@@ -29,10 +28,11 @@ function App() {
 
   return (
     <div className="App">
-      <div className="modal">
+      
         <h1>User Details Modal</h1>
         <button onClick={clickHandler}>Open Form</button>
         {isOpen && (
+        <div className="modal">
           <div className="modal-content" onClick={closeHandler}>
             <form onSubmit={submitHandler}>
               <h2>Fill Details</h2>
@@ -57,8 +57,9 @@ function App() {
               </button>
             </form>
           </div>
+          </div>
         )}
-      </div>
+      
     </div>
   );
 }
